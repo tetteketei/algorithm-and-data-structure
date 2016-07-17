57b03e533a5d939cb029981b7c8919b3c3643c30
Hikaru Komeiji <Hikaru@Hikarus-MBP.local>
2016-07-17 17:02:34 +0900

    add comment

--- sec2/alds1_1.cpp
+++ sec2/alds1_1.cpp
@@ -1,18 +1,21 @@
+//this program computes maximum profit in fx trading
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int main(){
-	int len;
-	cin >> len;
+	int len; //this is the length of period
 	int arr[len];
 	int minv,maxv;
+
+	cin >> len;
 	cin >> minv;
 	maxv = -1000000000;
+
 	int tmp;
 	for(int i = 1; i < len; i++){
-		cin >> tmp;	
-		maxv = max(maxv,tmp - minv);			
+		cin >> tmp;
+		maxv = max(maxv,tmp - minv);
 		minv = min(minv,tmp);
 	}
 	cout << maxv << endl;
